e8a82cab9f26a2bde87cb51b7eaeb4b876ba4e76
ankanbag101 <bagankan@gmail.com>
2022-05-30 23:50:52 +0530

    add: map and reduce functions to ArrayUtils

--- contracts/libs/ArrayUtils.sol
+++ contracts/libs/ArrayUtils.sol
@@ -2,25 +2,55 @@
 pragma solidity ^0.8.0;
 
 library ArrayUtils {
-    function includes(uint256[] storage _array, uint256 value)
+    function includes(uint256[] storage _array, uint256 _value)
         internal
         view
         returns (bool)
     {
         for (uint256 i = 0; i < _array.length; i++) {
-            if (_array[i] == value) return true;
+            if (_array[i] == _value) return true;
         }
         return false;
     }
 
-    function includes(address[] storage _array, address value)
+    function map(
+        uint256[] memory _array,
+        function(uint256) pure returns (uint256) _f
+    ) internal pure returns (uint256[] memory r) {
+        r = new uint256[](_array.length);
+        for (uint256 i = 0; i < _array.length; i++) {
+            r[i] = _f(_array[i]);
+        }
+    }
+
+    function reduce(
+        uint256[] memory _array,
+        function(uint256, uint256) pure returns (uint256) _f
+    ) internal pure returns (uint256 r) {
+        r = _array[0];
+        for (uint256 i = 1; i < _array.length; i++) {
+            r = _f(r, _array[i]);
+        }
+    }
+
+    function includes(address[] storage _array, address _value)
         internal
         view
         returns (bool)
     {
         for (uint256 i = 0; i < _array.length; i++) {
-            if (_array[i] == value) return true;
+            if (_array[i] == _value) return true;
         }
         return false;
+    }
+
+    function map(
+        address[] memory _array,
+        function(address) pure returns (address) _f
+    ) internal pure returns (address[] memory r) {
+        r = new address[](_array.length);
+        for (uint256 i = 0; i < _array.length; i++) {
+            r[i] = _f(_array[i]);
+        }
     }
 }
