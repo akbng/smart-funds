77abaf9c21d8693e978ea0598bee5f720798cd86
ankanbag101 <bagankan@gmail.com>
2022-05-31 21:31:50 +0530

    delete: members and Role from the Project

--- contracts/Projects.sol
+++ contracts/Projects.sol
@@ -2,83 +2,52 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Projects {
-    enum Role {
-        FOUNDER,
-        CO_FOUNDER,
-        CEO,
-        CFO,
-        CTO,
-        COO,
-        CMO,
-        PRESIDENT,
-        VICE_PRESIDENT,
-        EMPLOYEE
-    }
+  struct Milestone {
+    bytes32 name;
+    bytes description;
+    bool reached;
+    bytes proof; // hash returned from IPFS or swarm
+    uint64 expected_date;
+  }
 
-    struct Milestone {
-        bytes32 name;
-        bytes description;
-        bool reached;
-        bytes proof; // hash returned from IPFS or swarm
-        uint64 expected_date;
-    }
+  struct Project {
+    bytes32 name;
+    bytes description;
+    bytes main_video;
+    bytes pitch_deck;
+    uint32 target_fund;
+    uint32 raised_funds;
+    uint32 disbursted_funds;
+    address payable fund_address;
+    uint64 deadline;
+    address[] backers;
+    Milestone[] milestones;
+  }
 
-    struct PrivilegedUser {
-        bytes32 name;
-        Role position;
-        bytes32 project_name;
-        uint256 Fame;
-        bytes image;
-    }
+  Project[] projects;
 
-    struct Project {
-        bytes32 name;
-        bytes description;
-        bytes main_video;
-        bytes pitch_deck;
-        uint32 target_fund;
-        uint32 raised_funds;
-        uint32 disbursted_funds;
-        address payable fund_address;
-        uint64 deadline;
-        address[] backers;
-        Milestone[] milestones;
-        address[] members;
-    }
+  mapping(bytes32 => uint256) private projectIndex;
 
-    Project[] projects;
+  /// @dev checks if the Project Name provided is a valid Project or not
+  /// @param _projectName which is the bytes32 representation of the name of the Project
+  /// @return projectValidity of type bool. Returns true if the project is valid, false otherwise
+  function isValidProject(bytes32 _projectName) public view returns (bool) {
+    return getHash(getProject(_projectName).name) == getHash(_projectName);
+  }
 
-    mapping(address => PrivilegedUser) public specialUsers;
-    mapping(bytes32 => uint256) public projectIndex;
+  /// @dev getter function to get Project from the provided bytes32 Project name
+  /// @param _projectName which is the bytes32 representation of the name of a Project
+  /// @return project of type Project
+  function getProject(bytes32 _projectName)
+    internal
+    view
+    returns (Project memory)
+  {
+    bytes32 hash = getHash(_projectName);
+    return projects[projectIndex[hash]];
+  }
 
-    /// @dev checks if the Privileged user's address provided is a valid Priviliged user or not
-    /// @param _user which is the address of the Privileged user
-    /// @return userValidity of type bool. Returns true if the user is valid, false otherwise
-    function isValidPrivilegedUser(address _user) public view returns (bool) {
-        PrivilegedUser memory user = specialUsers[_user];
-        return user.name.length > 0 && isValidProject(user.project_name);
-    }
-
-    /// @dev checks if the Project Name provided is a valid Project or not
-    /// @param _projectName which is the bytes32 representation of the name of the Project
-    /// @return projectValidity of type bool. Returns true if the project is valid, false otherwise
-    function isValidProject(bytes32 _projectName) public view returns (bool) {
-        return getHash(getProject(_projectName).name) == getHash(_projectName);
-    }
-
-    /// @dev getter function to get Project from the provided bytes32 Project name
-    /// @param _projectName which is the bytes32 representation of the name of a Project
-    /// @return project of type Project
-    function getProject(bytes32 _projectName)
-        internal
-        view
-        returns (Project memory)
-    {
-        bytes32 hash = getHash(_projectName);
-        return projects[projectIndex[hash]];
-    }
-
-    function getHash(bytes32 _name) internal pure returns (bytes32) {
-        return sha256(abi.encode(_name));
-    }
+  function getHash(bytes32 _name) internal pure returns (bytes32) {
+    return sha256(abi.encode(_name));
+  }
 }
